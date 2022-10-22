5edaef7d0e21810bb602d501fde25f094fbca097
jiyoung <jen.jyseo@gmail.com>
2022-10-22 17:23:32 +0900

    add:: prettier 컨벤션 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 import Router from "./shared/Router";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { __getJoin } from "./redux/modules/joinSlice";
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(__getJoin());
+  }, [dispatch]);
+
   return (
     <>
       <Router />
---
+++ src/redux/modules/joinSlice.js
@@ -0,0 +1,35 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const __getJoin = createAsyncThunk(
+	"Join/getJoin",
+	async (_, thunkAPI) => {
+		try {
+			const data = await axios.get("http://54.180.79.123:8080/api/dummy");
+			console.log("data =>", data);
+			return thunkAPI.fulfillWithValue(data);
+		} catch (error) {
+			return thunkAPI.rejectWithValue(error);
+		}
+	},
+);
+
+const joinSlice = createSlice({
+	name: "join",
+	initialState: { join: null },
+	reducers: {},
+	extraReducers: {
+		[__getJoin.pending]: (state, _) => {},
+		[__getJoin.fulfilled]: (state, action) => {
+			console.log("action.payload =>", action.payload);
+			state.join = action.payload;
+		},
+		[__getJoin.rejected]: (state, action) => {
+			console.log("__getJoin.rejected", action.payload);
+			state.join = action.payload;
+		},
+	},
+});
+
+export const {} = joinSlice.actions;
+export default joinSlice.reducer;
