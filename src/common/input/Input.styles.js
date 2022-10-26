b140a92acdfe74fe826ce295654eb0d9d08e2082
NyeongDev <90minyeong90@gmail.com>
2022-10-26 23:45:30 +0900

    _

--- src/common/input/Input.styles.js
+++ src/common/input/Input.styles.js
@@ -1,3 +1,30 @@
 import styled from "styled-components";
 
-export const Input = styled.input``;
+export const Input = styled.input`
+	border: 1px solid #42364b;
+	border-radius: 5px;
+	height: 50px;
+
+	${({ size }) => {
+		switch (size) {
+			case "long":
+				return `
+        width: 480px;
+        `;
+			case "short":
+				return `
+        width: 360px;
+        `;
+			case "post":
+				return `
+        650px;
+        `;
+			case "comment":
+				return `
+        width: 600px;
+        `;
+			default:
+				break;
+		}
+	}};
+`;
